5a22bf132b18c0bae1cea6b391c51ad8404d4de8
PetarMishov <pm13167@student.uni-lj.si>
2024-05-13 22:38:37 +0200

    Added tests for sphere intersection.

--- tests.py
+++ tests.py
@@ -10,8 +10,8 @@ class Tester(unittest.TestCase):
             func()
         except:
             return self.fail() 
-    def testEqual(self, first, second):
-        self.assertEqual(first, second)
+    def testAlmostEqual(self, first, second):
+        self.assertAlmostEqual(first, second, places = 8)
 
 def initalization_test_1():
     ray = Ray(np.array([]), np.array([]))
@@ -32,7 +32,27 @@ def initalization_test_4():
 def intersection_test_1():
     ray = Ray(np.array([0,4,0]), np.array([0,-1,0]))
     sphere = Sphere(np.array([0,0,0]), 2)
-    return ray.sphere_intersect(sphere)
+    return ray.sphere_intersect(sphere) #t=2
+
+def intersection_test_2():
+    ray = Ray(np.array([0,10,0]), np.array([1,1,1]))
+    sphere = Sphere(np.array([1,2,3]), 2)
+    return ray.sphere_intersect(sphere) #doesn't intersect
+
+def intersection_test_3():
+    ray = Ray(np.array([13,-6,0]), np.array([1,-1,0]))
+    sphere = Sphere(np.array([3,2,0]), 2)
+    return ray.sphere_intersect(sphere) #sphere behind camera
+
+def intersection_test_4():
+    ray = Ray(np.array([13,-6,0]), np.array([-1/np.sqrt(2),1/np.sqrt(2),0]))
+    sphere = Sphere(np.array([3,2,0]), 2)
+    return ray.sphere_intersect(sphere) #t=8 root 2
+
+def intersection_test_5():
+    ray = Ray(np.array([1,1,1]), np.array([4,3,2]))
+    sphere = Sphere(np.array([6,5,4]), np.sqrt(3))
+    return ray.sphere_intersect(sphere) 
 
 tester = Tester()
 
@@ -40,4 +60,10 @@ tester.testRaises(InvalidDimension, initalization_test_1)
 tester.testRaises(InvalidArrayDimensionPos, initalization_test_2)
 tester.testRaises(TypeError, initalization_test_3)
 tester.testNoRaise(initalization_test_4)
-tester.testEqual(2, intersection_test_1())
+
+tester.testAlmostEqual(2, intersection_test_1())
+tester.testAlmostEqual(None, intersection_test_2())
+tester.testAlmostEqual(None, intersection_test_3()) 
+tester.testAlmostEqual(8*np.sqrt(2), intersection_test_4()) 
+tester.testAlmostEqual(5.3851648071, intersection_test_5()) #calculated manually with wolfram alpha
+
