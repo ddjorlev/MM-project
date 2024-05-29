3fa6b9f0a58f03e9825b5ee26cce58567afcfe34
babusot <milos.mitrev@yahoo.com>
2024-05-29 20:29:31 +0200

    class for cone

--- classesNewton.py
+++ classesNewton.py
@@ -70,6 +70,136 @@ class Sphere:
     def center(self):
         c = np.array([self.pos[0], self.pos[1], self.pos[2]])
         return c
+    
+
+class Cone:
+    def __init__(self, pos : np.array, radius, color : np.array, illum : Illum, shininess, reflection : float, camera: np.array):
+        #pos = [x,y,z]
+        self.pos = pos
+        self.illum = illum
+        self.radius = radius
+        self.color = color
+        self.shininess = shininess
+        self.reflection = reflection
+        self.camera=camera
+
+    #Function for calculating the value of a cone in the point x[x1, y1, z1]
+    def calculateF(self, x: np.array):
+        if x[0] == 0:
+            firstPart =0
+        else:
+            firstPart=(self.pos[0]**2)/(x[0]**2)
+        
+        if x[1] == 0:
+            secondPart =0
+        else:
+            secondPart=(self.pos[1]**2)/(x[1]**2)
+        
+        if x[2] == 0:
+            thirdPart =0
+        else:
+            thirdPart=(self.pos[2]**2)/(x[2]**2)
+        
+
+        return firstPart + secondPart - thirdPart
+    
+    #Function for calculating the first derrivative of a cone in the point t[x, y, z]
+    def dx(self, t:np.array) :
+        return 2*self.pos[0]/(t[0] **2)
+    
+    #Function for calculating the second derrivative of a cone in the point t[x, y, z]
+    def dy(self, t:np.array) :
+        return  2*self.pos[1]/(t[1] **2)
+    
+    #Function for calculating the third derrivative of a cone in the point t[x, y, z]
+    def dz(self, t:np.array) :
+        return  -2*self.pos[2]/(t[2] **2)
+    
+    #Function g(t):=f(x0 + a*t, y0 + a*t, z0 + a*t)
+    #used to find the exact point of intersection
+    def g(self, t:np.array, lineDirection:np.array) :
+        newX=self.camera[0]+t*lineDirection[0]
+        newY=self.camera[1]+t*lineDirection[1]
+        newZ=self.camera[2]+t*lineDirection[2]
+                                                 
+        return self.calculateF(np.array([newX, newY, newZ]))
+    
+    #Derrivative of the function above
+    def gdot(self, t:np.array, lineDirection:np.array) :
+        return (
+            lineDirection[0] * self.dx(np.array([self.camera[0] +t*lineDirection[0], self.camera[1]+t*lineDirection[1], self.camera[2]+t*lineDirection[2]])) +
+            lineDirection[1] * self.dy(np.array([self.camera[0] +t*lineDirection[0], self.camera[1]+t*lineDirection[1], self.camera[2]+t*lineDirection[2]])) +
+            lineDirection[2] * self.dz(np.array([self.camera[0] +t*lineDirection[0], self.camera[1]+t*lineDirection[1], self.camera[2]+t*lineDirection[2]]))
+            )
+    
+    #function for calculating the sign in the point t[x, y, z]
+    def sign(self, t:np.array):
+        s = np.sign(self.calculateF(t))
+        if s == 0:
+            s=1
+        return s
+    
+    #Returning the center of the sphere
+    def center(self):
+        c = np.array([self.pos[0], self.pos[1], self.pos[2]])
+        return c
+    
+
+class Cylinder:
+    def __init__(self, pos : np.array, radius, color : np.array, illum : Illum, shininess, reflection : float, camera: np.array):
+        #pos = [x,y,z]
+        self.pos = pos
+        self.illum = illum
+        self.radius = radius
+        self.color = color
+        self.shininess = shininess
+        self.reflection = reflection
+        self.camera=camera
+
+    #Function for calculating the value of a sphere in the point x[x1, y1, z1]
+    def calculateF(self, x: np.array):
+        return (self.pos[0]**2)/(x[0]**2) + (self.pos[1]**2)/(x[1]**2) - (self.pos[2]**2)/(x[2]**2) 
+    
+    #Function for calculating the first derrivative of a sphere in the point t[x, y, z]
+    def dx(self, t:np.array) :
+        return 2*self.pos[0]/(t[0] **2)
+    
+    #Function for calculating the second derrivative of a sphere in the point t[x, y, z]
+    def dy(self, t:np.array) :
+        return  2*self.pos[1]/(t[1] **2)
+    
+    #Function for calculating the third derrivative of a sphere in the point t[x, y, z]
+    def dz(self, t:np.array) :
+        return  -2*self.pos[2]/(t[2] **2)
+    
+    #Function g(t):=f(x0 + a*t, y0 + a*t, z0 + a*t)
+    #used to find the exact point of intersection
+    def g(self, t:np.array, lineDirection:np.array) :
+        newX=self.camera[0]+t*lineDirection[0]
+        newY=self.camera[1]+t*lineDirection[1]
+        newZ=self.camera[2]+t*lineDirection[2]
+                                                 
+        return self.calculateF(np.array([newX, newY, newZ]))
+    
+    #Derrivative of the function above
+    def gdot(self, t:np.array, lineDirection:np.array) :
+        return (
+            lineDirection[0] * self.dx(np.array([self.camera[0] +t*lineDirection[0], self.camera[1]+t*lineDirection[1], self.camera[2]+t*lineDirection[2]])) +
+            lineDirection[1] * self.dy(np.array([self.camera[0] +t*lineDirection[0], self.camera[1]+t*lineDirection[1], self.camera[2]+t*lineDirection[2]])) +
+            lineDirection[2] * self.dz(np.array([self.camera[0] +t*lineDirection[0], self.camera[1]+t*lineDirection[1], self.camera[2]+t*lineDirection[2]]))
+            )
+    
+    #function for calculating the sign in the point t[x, y, z]
+    def sign(self, t:np.array):
+        s = np.sign(self.calculateF(t))
+        if s == 0:
+            s=1
+        return s
+    
+    #Returning the center of the sphere
+    def center(self):
+        c = np.array([self.pos[0], self.pos[1], self.pos[2]])
+        return c
 
 
 def Newton(s: Sphere, lineDirection: np.array, x0:float ,epsilon,max_iter):
@@ -170,7 +300,11 @@ def Ray(itMax: int, stepsize: int, camera: np.array, lineDirection:np.array, sph
         
         centerOfTheSphere=spheres[idSphere].center()
 
-        normal = normalize(betterPoint - centerOfTheSphere)
+        #normal = normalize(betterPoint - centerOfTheSphere)
+        
+        normal = np.array([spheres[idSphere].dx(betterPoint), spheres[idSphere].dy(betterPoint), spheres[idSphere].dz(betterPoint)])
+        normal = normalize(normal)
+        
         intersection_to_light = normalize(light.pos - betterPoint)
         
         colorToAdd += spheres[idSphere].illum.diffuse * light.illum.diffuse * np.dot(intersection_to_light,normal)
--- raytracingNewton.py
+++ raytracingNewton.py
@@ -1,10 +1,10 @@
 import numpy as np
-from classesNewton import  Illum, Sphere, Newton, normalize, Light, Ray
+from classesNewton import  Illum, Sphere, Newton, normalize, Light, Ray, Cone
 import matplotlib.pyplot as plt
 
 
-width = 200
-height = 200
+width = 100
+height = 100
 
 #pixels = np.empty(shape=(height, width, 3))
 pixels = np.zeros(shape=(height, width, 3))
@@ -23,8 +23,9 @@ height_pos = [-ratio, ratio] #minimum and maximum height
 
 
 #pos : np.array, radius, color : np.array, illum : Illum, shininess, reflection : float, camera: np.array, lineDirection: np.array):
-functions = [Sphere(np.array([-0.5, 0, -1]), 0.7, np.array([57/256, 68/256, 188/256]), Illum(np.array([1,1,1]), np.array([1,1,1]), np.array([1,1,1])), 80, 0.4, camera),
-            Sphere(np.array([0.2, -0.3, -2]), 0.7, np.array([123/256, 6/256, 35/256]), Illum(np.array([1,1,1]), np.array([1,1,1]), np.array([1,1,1])), 80, 0.4, camera),
+functions = [Sphere(np.array([-5, -5, -5]), 0.7, np.array([57/256, 68/256, 188/256]), Illum(np.array([1,1,1]), np.array([1,1,1]), np.array([1,1,1])), 80, 0.4, camera),
+           # Sphere(np.array([0.2, -0.3, -2]), 0.7, np.array([123/256, 6/256, 35/256]), Illum(np.array([1,1,1]), np.array([1,1,1]), np.array([1,1,1])), 80, 0.4, camera),
+            Cone(np.array([0.2, -0.3, 0.1]), 0.7, np.array([123/256, 6/256, 35/256]), Illum(np.array([1,1,1]), np.array([1,1,1]), np.array([1,1,1])), 80, 0.4, camera),
             Sphere(np.array([0, -1000, 0]), 999, np.array([150/256, 70/256, 150/256]), Illum(np.array([0.1, 0.1, 0.1]), np.array([0.6, 0.6, 0.6]), np.array([1,1,1])), 80, 0.1, camera)]
 
 for i, y in enumerate(np.linspace(height_pos[0], height_pos[1], height)):
@@ -42,7 +43,9 @@ for i, y in enumerate(np.linspace(height_pos[0], height_pos[1], height)):
             colorToBePlaced, betterPoint, obj_reflection, intersection, idFunction = Ray(250, 0.01, camera, direction, functions, Newton, light, cumulative_reflection)
             if intersection == 1:
                 cumulative_reflection *= obj_reflection
-                normal = normalize(betterPoint - functions[idFunction].center())
+               # normal = normalize(betterPoint - functions[idFunction].center())
+                normal = np.array([functions[idFunction].dx(betterPoint), functions[idFunction].dy(betterPoint), functions[idFunction].dz(betterPoint)])
+                normal = normalize(normal)
                 direction = direction - 2 * np.dot(direction, normal) * normal
                 origin = betterPoint
                 pixels[height-i-1, j-1] += colorToBePlaced
